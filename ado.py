c6646144bb3bd4f04c5240a6fcd5a7060be3156c
chad.hodnett <chad.hodnett@nucor.com>
2024-03-27 11:57:14 -0500

    Dead end on stale releases

--- ado.py
+++ ado.py
@@ -1,14 +1,17 @@
-from azure.devops.connection import Connection
-from msrest.authentication import BasicAuthentication
+from datetime import datetime, timedelta, timezone
+from azure.devops.v7_0.release.release_client import ReleaseClient
+
+PROJECT = "2d1ccb81-c104-4f43-9181-25603aff23ef"
+ACTIVE = "active"
 
 
-def labels_without_releases(release_client, labels, release_definition):
+def labels_without_releases(release_client: ReleaseClient, labels, release_definition):
     missing = []
     for label in labels:
         releases = release_client.get_releases(
-            project="2d1ccb81-c104-4f43-9181-25603aff23ef",
+            project=PROJECT,
             is_deleted=False,
-            status_filter="active",
+            status_filter=ACTIVE,
             search_text=label,
         )
 
@@ -17,7 +20,53 @@ def labels_without_releases(release_client, labels, release_definition):
         ]
 
         if not releases:
-            print(f"Found no release for nsbb {label}.")
+            print(
+                f"Found no release for {label} in release definition {release_definition}."
+            )
             missing.append(label)
 
     return missing
+
+
+def get_recent_deleted_releases(release_client: ReleaseClient, release_definition):
+    return [
+        r
+        for r in release_client.get_releases(
+            project=PROJECT,
+            status_filter=ACTIVE,
+            is_deleted=True,
+        )
+        if r.release_definition.id == release_definition
+    ]
+
+
+def get_stale_releases(
+    release_client: ReleaseClient, release_definition, inactive_days=23
+):
+    """
+    BAD - modified time does not include deployments. This would require
+    figuring out the last deployment for a release which is not supported
+
+    https://developercommunity.visualstudio.com/t/how-to-get-latest-version-of-deployment-using-rest/1161013
+    """
+    releases = release_client.get_releases(
+        project=PROJECT,
+        status_filter=ACTIVE,
+        is_deleted=False,
+    )
+    while release_client.continuation_token_last_request is not None:
+        releases += release_client.get_releases(
+            project=PROJECT,
+            status_filter=ACTIVE,
+            is_deleted=False,
+            continuation_token=release_client.continuation_token_last_request,
+        )
+
+    print(f"Found {len(releases)} stale releases before filter")
+
+    return [
+        r
+        for r in releases
+        if r.release_definition.id == release_definition
+        and datetime.now(timezone.utc) - r.modified_on > timedelta(days=inactive_days)
+    ]
